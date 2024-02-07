47ef9d5c052ed44fa172f71c76f41d610993dc3c
vallabhs1216 <100321435+vallabhs1216@users.noreply.github.com>
2024-02-08 12:54:12 +1300

    Added yes/no checker.

---
+++ 01_yes_no.py
@@ -0,0 +1,17 @@
+
+
+# ask the user if they have played before
+want_instructions = input("Do you want instructions ").lower()
+
+# If they say yes, output 'display instructions'
+# If they say no, output 'program continues'
+# If they say anything else, outputs error
+
+if want_instructions == "yes" or want_instructions == "y":
+    print("Instructions")
+
+elif want_instructions == "no" or want_instructions == "n":
+    print("Program continues")
+
+else:
+    print("Please answer yes / no")
